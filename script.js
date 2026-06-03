82b4e145cfd1890497805f95f793fc05609771e6
Alexander Chang <alexanderchang208@gmail.com>
2026-06-02 22:17:08 -0500

    revamp reconstruction

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-const carousel = document.querySelector('.carousel');
-const cards = document.querySelectorAll('.card');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-
-const totalCards = cards.length;
-const theta = 360 / totalCards; 
-
-const cardWidth = 300; 
-const radius = Math.round((cardWidth / 2) / Math.tan(Math.PI / totalCards));
-
-let currentIndex = 0;
-
-cards.forEach((card, index) => {
-  const cardAngle = theta * index;
-  card.style.transform = `rotateY(${cardAngle}deg) translateZ(${radius}px)`;
-});
-
-function rotateCarousel() {
-  const angle = theta * currentIndex * -1;
-  carousel.style.transform = `translateZ(-${radius}px) rotateY(${angle}deg)`;
+function onloadFunction () {
+  setTimeout(function () {
+    document.getElementById("load-asset").style.display = "none";
+  }, 500);
 }
 
-prevBtn.addEventListener('click', () => {
-  currentIndex--;
-  rotateCarousel();
-});
-
-nextBtn.addEventListener('click', () => {
-  currentIndex++;
-  rotateCarousel();
-});
+window.addEventListener('load', onloadFunction);
